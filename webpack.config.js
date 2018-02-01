160ae31e954e7e29312b4c0998e1de3b89cccda9
hujiao <hujiao@meituan.com>
2018-02-01 11:06:34 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const common = require('./webpack/common.js');
 const webpackMerge = require('webpack-merge');
 
 const config = {
-    entry: {'easy-console.min': './lib/index.js'},
+    entry: {'easy-console-model.min': './lib/index.js'},
 };
 
 module.exports = webpackMerge(common, config);
